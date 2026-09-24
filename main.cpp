9d03f3c405ba18d02f23c062a0e4aa6de97b23c8
Hessvacio Hassan <madeinbrooklyngames@gmail.com>
2026-09-24 00:16:28 -0400

    Remove commented TODOs from main.cpp

--- main.cpp
+++ main.cpp
@@ -2,15 +2,7 @@
 using namespace std;
 
 int main() {
-    //TODO: Create int var and cout and cin commands
-
-
-    // TODO: Write a for loop that runs from 1 to limit
-    
-
-        // TODO: Use if/else statements to apply FizzBuzz logic
-
-    
+    //TODO: Create int var and cout and cin commands    
 
     return 0;
 }
